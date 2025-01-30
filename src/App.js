79e3213a134b1cbd331f0f23b0624d369f1b6ebe
Vikash Keshri <vkeshri.14@gmail.com>
2025-01-31 00:52:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { Suspense } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/frontend/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router basename="/">
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/frontend/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/frontend/global/navbar";
+import Footer_v1 from "../../components/frontend/global/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Footer_v1 />
+    </div>
+  );
+}
