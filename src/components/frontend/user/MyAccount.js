803ca45783caecf8d4a745d2a49d1f359605c3c3
Vikash Keshri <vkeshri.14@gmail.com>
2025-02-24 19:48:11 +0300

    update

--- src/components/frontend/user/MyAccount.js
+++ src/components/frontend/user/MyAccount.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as ApiService from "../../../config/config";
 import apiList from "../../../config/apiList.json";
 import { Link } from "react-router-dom";
@@ -6,19 +6,95 @@ import config from "../../../config/config.json";
 import SideBar from "./SideBar";
 import { useSelector } from "react-redux";
 export default function MyAccount() {
+  const userinfo = useSelector((state) => state.user.items);
   const [pharmacyName, setPharmacyName] = useState("");
   const [pharmacyOwner, setPharmacyOwner] = useState("");
   const [email, setEmail] = useState("");
   const [whatsappno, setWhatsappno] = useState("");
   const [phoneNo, setPhoneNo] = useState("");
   const [pharmacyType, setPharmacyType] = useState("");
-
+  const [tax, setTax] = useState("");
+  const [taxError, setTaxError] = useState("");
   const [pharmacyNameError, setPharmacyNameError] = useState("");
   const [pharmacyOwnerError, setPharmacyOwnerError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [whatsappnoError, setWhatsappnoError] = useState("");
   const [phoneNoError, setPhoneNoError] = useState("");
   const [pharmacyTypeError, setPharmacyTypeError] = useState("");
+
+  useEffect(() => {
+    const fetchItemById = async () => {
+      const obj = {
+        id: userinfo.id,
+      };
+      let params = { url: apiList.getUserById, body: obj }; //console.log(params);
+      let response = await ApiService.postData(params);
+      const data = await response.results;
+      if (data.length) setEmail(data[0].email);
+      setPharmacyName(data[0].pharmacy_name);
+      setPharmacyOwner(data[0].pharmacy_owner_name);
+      setWhatsappno(data[0].whatsapp);
+      setPhoneNo(data[0].phone);
+      setTax(data[0].tax_no);
+      setPharmacyType(data[0].pharmacy_type);
+    };
+    fetchItemById();
+  }, []);
+
+  const updateProfile = async () => {
+    if (!pharmacyName) {
+      setPharmacyNameError(true);
+      return;
+    }
+    setPharmacyNameError(false);
+    if (!pharmacyOwner) {
+      setPharmacyOwnerError(true);
+      return;
+    }
+    setPharmacyOwnerError(false);
+
+    if (!email) {
+      setEmailError(true);
+      return;
+    }
+    setEmailError(false);
+    if (!whatsappno) {
+      setWhatsappnoError(true);
+      return;
+    }
+    setWhatsappnoError(false);
+    if (!phoneNo) {
+      setPhoneNoError(true);
+      return;
+    }
+    setPhoneNoError(false);
+    if (!tax) {
+      setTaxError(true);
+      return;
+    }
+    setTaxError(false);
+    if (!tax) {
+      setTaxError(true);
+      return;
+    }
+    setTaxError(false);
+
+    if (!pharmacyType) {
+      setPharmacyTypeError(true);
+      return;
+    }
+    setPharmacyTypeError(false);
+
+
+    const obj={
+      pharmacy_name:,
+      pharmacy_owner_name:,
+      whatsapp:,
+      tax_no:,
+      pharmacy_type:
+      id:userinfo.id
+    }
+  };
 
   return (
     <div className="liton__wishlist-area pb-70">
@@ -39,14 +115,12 @@ export default function MyAccount() {
                         id="ltn_tab_1_4"
                       >
                         <div className="ltn__myaccount-tab-content-inner">
-                          <p>
-                            The following addresses will be used on the checkout
-                            page by default.
-                          </p>
+                          <h3>Personal Information</h3>
+
                           <div className="ltn__form-box">
                             <form action="#">
                               <div className="row mb-50">
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
                                   <label>Pharmacy name:</label>
                                   <input
                                     onChange={(e) =>
@@ -55,9 +129,12 @@ export default function MyAccount() {
                                     value={pharmacyName}
                                     type="text"
                                     name="ltn__name"
+                                    className={
+                                      pharmacyNameError ? "border-danger" : ""
+                                    }
                                   />
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
                                   <label>Pharmacy Owner:</label>
                                   <input
                                     onChange={(e) =>
@@ -66,19 +143,25 @@ export default function MyAccount() {
                                     value={pharmacyOwner}
                                     type="text"
                                     name="ltn__lastname"
+                                    className={
+                                      pharmacyOwnerError ? "border-danger" : ""
+                                    }
                                   />
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
                                   <label>Email:</label>
                                   <input
                                     onChange={(e) => setEmail(e.target.value)}
                                     value={email}
-                                    type="text"
+                                    type="email"
                                     name="ltn__lastname"
-                                    placeholder="Ethan"
+                                    placeholder="Email"
+                                    className={
+                                      emailError ? "border-danger" : ""
+                                    }
                                   />
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
                                   <label>Whatsapp No:</label>
                                   <input
                                     onChange={(e) =>
@@ -87,22 +170,36 @@ export default function MyAccount() {
                                     value={whatsappno}
                                     type="text"
                                     name="ltn__lastname"
-                                    placeholder="example@example.com"
+                                    placeholder="Whatsapp number"
+                                    className={
+                                      whatsappnoError ? "border-danger" : ""
+                                    }
                                   />
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
                                   <label>Phone No:</label>
                                   <input
                                     onChange={(e) => setPhoneNo(e.target.value)}
                                     value={phoneNo}
-                                    type="email"
+                                    type="text"
                                     name="ltn__lastname"
-                                    placeholder="example@example.com"
+                                    placeholder="Phone number"
                                   />
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-6 mb-3">
+                                  <label>Tax No:</label>
+                                  <input
+                                    onChange={(e) => setTax(e.target.value)}
+                                    value={tax}
+                                    type="text"
+                                    name="ltn__lastname"
+                                    placeholder="Tax No"
+                                  />
+                                </div>
+                                <div className="col-md-6 mb-3">
                                   <label>Pharmacy Type:</label>
                                   <select
+                                    className="form-control"
                                     onChange={(e) =>
                                       setPharmacyType(e.target.value)
                                     }
@@ -119,8 +216,9 @@ export default function MyAccount() {
 
                               <div className="btn-wrapper">
                                 <button
-                                  type="submit"
-                                  className="btn theme-btn-1 btn-primary btn-effect-1 text-uppercase"
+                                  type="button"
+                                  onClick={() => updateProfile()}
+                                  className="btn theme-btn-1 btn-primary btn-effect text-uppercase"
                                 >
                                   Save Changes
                                 </button>
